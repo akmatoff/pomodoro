e573f51a1f7492657a3caadde11fad4ed02fa396
akmatoff <azim.akmatov1@gmail.com>
2021-08-11 19:26:19 +0600

    UPDATE: Button hover, start timer, update time

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
+import { useState } from "react";
+import { padTime } from "./helpers";
 import "./App.css";
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
+  const startTimer = () => {
+    setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft > 1) return timeLeft - 1;
+      });
+    }, 1000);
+  };
+
   return (
     <div className="App">
       <main className="main flex-column centered">
         <div className="pomodoro-container flex-column">
           <h1 className="title">Pomodoro</h1>
           <div className="timer">
-            <span>00</span>
+            <span>{minutes}</span>
             <span>:</span>
-            <span>00</span>
+            <span>{seconds}</span>
           </div>
           <div className="buttons flex-row">
-            <button className="button" id="start-or-pause">
+            <button className="button" id="start-or-pause" onClick={startTimer}>
               Start
             </button>
             <button className="button" id="stop">
